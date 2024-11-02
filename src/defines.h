83e033e074202295776c28c6fcb69b6a2555ed02
Kurashizu <kurashizu123@gmail.com>
2024-11-02 14:21:25 +0800

    publish

---
+++ src/defines.h
@@ -0,0 +1,29 @@
+#define IMU IMU_BMI160
+#define SECOND_IMU IMU
+#define BOARD BOARD_CUSTOM
+#define IMU_ROTATION DEG_180
+#define SECOND_IMU_ROTATION DEG_90
+
+#define PRIMARY_IMU_OPTIONAL false
+#define SECONDARY_IMU_OPTIONAL true
+
+#define MAX_IMU_COUNT 1
+
+#ifndef IMU_DESC_LIST
+#define IMU_DESC_LIST \
+    IMU_DESC_ENTRY(IMU,        PRIMARY_IMU_ADDRESS_ONE,   IMU_ROTATION,        PIN_IMU_SCL, PIN_IMU_SDA, PRIMARY_IMU_OPTIONAL,   PIN_IMU_INT) 
+
+#endif
+
+// Battery monitoring options (comment to disable):
+// BAT_EXTERNAL for ADC pin, BAT_INTERNAL for internal - can detect only low battery, BAT_MCP3021 for external ADC
+#define BATTERY_MONITOR BAT_EXTERNAL
+#define BATTERY_SHIELD_RESISTANCE 0 //130k BatteryShield, 180k SlimeVR or fill in external resistor value in kOhm
+#define BATTERY_SHIELD_R1 10 // Board voltage divider resistor Ain to GND in kOhm
+#define BATTERY_SHIELD_R2 180 // Board voltage divider resistor Ain to INPUT_BOARD in kOhm
+
+#define PIN_IMU_SDA D2
+#define PIN_IMU_SCL D1
+#define PIN_IMU_INT D5
+#define PIN_IMU_INT_2 D6
+#define PIN_BATTERY_LEVEL A0
